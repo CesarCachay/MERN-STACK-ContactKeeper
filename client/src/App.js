1cff01e16b767bcf22fd8660f4869c886509b04c
Cesar Cachay <cesarcachay1996@gmail.com>
2019-11-04 00:20:17 -0500

    Add router and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./views/Home";
+import About from "./views/About";
 
 const App = () => {
-  return <div className="App">ContactKeeper App</div>;
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </>
+    </Router>
+  );
 };
 
 export default App;
