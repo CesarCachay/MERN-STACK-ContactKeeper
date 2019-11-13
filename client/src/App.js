7391a108a4ecee66750172b2182511dbdc86689d
Cesar Cachay <cesarcachay1996@gmail.com>
2019-11-13 00:00:14 -0500

    Show alert in registration form

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/layout/Navbar";
 import Home from "./views/Home";
 import About from "./views/About";
 import Register from "./views/Register";
 import Login from "./views/Login";
 import ContactState from "./context/contact/ContactState";
 import AuthContext from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
+import Alerts from "./components/layout/Alerts";
 
 const App = () => {
   return (
     <AuthContext>
       <ContactState>
-        <Router>
-          <>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-              </Switch>
-            </div>
-          </>
-        </Router>
+        <AlertState>
+          <Router>
+            <>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
+                </Switch>
+              </div>
+            </>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthContext>
   );
--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "../context/alert/AlertContext";
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -14,9 +19,15 @@ const Register = () => {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = e => {
-    e.prevenDefault();
-    console.log("Register submitted!");
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please enter all fields!", "danger");
+    } else if (password !== password2) {
+      setAlert("Password do not match!", "danger");
+    } else {
+      console.log("User created!");
+    }
   };
 
   return (
@@ -45,6 +56,8 @@ const Register = () => {
             name="password"
             value={password}
             onChange={handleChange}
+            required
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -54,6 +67,8 @@ const Register = () => {
             name="password2"
             value={password2}
             onChange={handleChange}
+            required
+            minLength="6"
           />
         </div>
         <input
