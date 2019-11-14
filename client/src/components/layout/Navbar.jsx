ab7c094059695f0c76fc9a1f346208204611264e
Cesar Cachay <cesarcachay1996@gmail.com>
2019-11-14 01:04:49 -0500

    Add logout functionality

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useContext, Fragment } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/AuthContext";
 
 const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+
+  const { isAuthenticated, logoutUser, user } = authContext;
+
+  const onLogout = () => {
+    logoutUser();
+  };
+
+  const loggedLinks = (
+    <Fragment>
+      <li>Welcome {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href="#!">
+          <i style={{ color: "red" }} className="fas fa-sign-out-alt"></i>{" "}
+          <span style={{ color: "red" }} className="hide-sm">
+            Logout
+          </span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </Fragment>
+  );
+
   return (
     <div className="navbar bg-primary">
       <h1>
@@ -10,20 +44,7 @@ const Navbar = ({ title, icon }) => {
           <i className={icon} /> {title}
         </a>
       </h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
+      <ul>{isAuthenticated ? loggedLinks : guestLinks}</ul>
     </div>
   );
 };
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -94,7 +94,7 @@ const AuthState = props => {
   };
 
   // Logout
-  const logoutUser = () => console.log("You logged out");
+  const logoutUser = () => dispatch({ type: LOGOUT });
 
   // Clear Errors
   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
