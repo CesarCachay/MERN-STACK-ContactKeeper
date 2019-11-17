4d04149c7bf3e3d845845d4e0aa75343cbfb9aad
Cesar Cachay <cesarcachay1996@gmail.com>
2019-11-17 01:04:12 -0500

    Prepare to deploy in Heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -12,6 +13,17 @@ app.use(express.json({ extended: false }));
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContacKeeper API" })
 );
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  //Set the routes. __dirname means that will look for the current directory and this will look first for client, then for build and finally index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 //Define routes
 app.use("/api/users", require("./routes/users"));
